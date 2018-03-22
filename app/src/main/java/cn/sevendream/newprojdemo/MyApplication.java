44d90d91c32b7307df747a35efa2d9c5fa5934d0
zhangxue <>
2018-03-22 19:43:52 +0800

    Initial commit

---
+++ app/src/main/java/cn/sevendream/newprojdemo/MyApplication.java
@@ -0,0 +1,14 @@
+package cn.sevendream.newprojdemo;
+
+import android.app.Application;
+
+/**
+ * Created by zhangxue on 2018/3/22.
+ */
+
+public class MyApplication extends Application {
+	@Override
+	public void onCreate() {
+		super.onCreate();
+	}
+}
